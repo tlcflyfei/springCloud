52c13ea9fdc728afa5a552301bea144135801b24
chang.liu <253036603@qq.com>
2019-03-10 23:30:42 +0800

    gateway

---
+++ gateway/src/main/java/com/wish/gateway/route/Route.java
@@ -0,0 +1,24 @@
+package com.wish.gateway.route;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lc
+ * @describe 功能描述
+ * @Date 21:28 on 2019/3/10
+ */
+@RestController
+public class Route {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().
+                route(predicateSpec -> predicateSpec.path("get").
+                        filters(gatewayFilterSpec -> gatewayFilterSpec.addRequestHeader("mykey", "myvalue")).
+                        uri("http://springCloud-gateway.com:80")).
+                build();
+    }
+}
