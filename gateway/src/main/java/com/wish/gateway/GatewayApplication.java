d3175066c26fb7319e4b9916e4ce1619c618ac18
changliu <changliu@ips.com>
2019-03-11 16:53:37 +0800

    调整boot版本

--- gateway/src/main/java/com/wish/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/wish/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.wish.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,12 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().
+                route(predicateSpec -> predicateSpec.path("/get").
+                        filters(gatewayFilterSpec -> gatewayFilterSpec.addRequestHeader("mykey", "myvalue")).
+                        uri("http://httpbin.org/get")).
+                build();
+    }
 }
